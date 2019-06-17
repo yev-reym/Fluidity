cbfc27618f99ebe0db95506a0bfd1d5545e94828
yevgeniyreym <yevgeniyreym@gmail.com>
2019-06-17 01:33:19 -0400

    Begin rendering function main and change details to mesh, and add constants file for canvas scale proportional to clipspace

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 // webpack.config.js
 var path = require('path');
+const ROOT_PATH = path.resolve(__dirname);
 
-module.exports = {
+module.exports = { 
     entry: './lib/fluidity.js',
     output: {
         path: path.resolve(__dirname),
         filename: 'bundle.js'
     },
-    devtool: 'source-map',
     resolve: {
         extensions: ['.js', '*']
-    }
+    },
+    module: {
+        rules: [
+             {
+                test: /\.glsl$/,
+                include: path.resolve(ROOT_PATH, 'lib/shader'),
+                loader: 'webpack-glsl-loader'
+            },
+            { test: /\.png$/, use: 'url-loader?mimetype=image/png' }
+        ]
+    },
+    devtool: 'source-map'
 };
